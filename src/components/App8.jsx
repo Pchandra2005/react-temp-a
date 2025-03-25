6ab14790b9fe31a14e04ebd519b41e398fc7e7bb
Prabhas2005 <prabhaschandra2005@gmail.com>
2025-03-25 15:26:53 +0530

    6th

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { useState, useEffect } from "react";
 export default function App8() {
-    const [a,setA] = useState(0);
-    const [total,setTotal] = useState(0);
-
-    const handleTotal = () => {
-        setTotal(Number(total) + Number(a))
-    };
-    useEffect(() => {
-        handleTotal();
-    },[a])
-    return (
-        <div>
-            <h3>This is App8</h3>
-            <p>
-                <input
-                 type="number"
-                 onChange={(e) => {setA(e.target.value)}}
-                 ></input>
-            </p>
-            <p>
-                {total}
-            </p>
-        </div>
+  const [numbers, setNumbers] = useState([]);
+  const [number, setNumber] = useState();
+  const [total, setTotal] = useState();
+  const handleSubmit = () => {
+    setNumbers([...numbers, number]);
+  };
+  useEffect(() => {
+    setTotal(
+      numbers.reduce((sum, value) => {
+        return sum + Number(value);
+      }, 0)
     );
+  }, [numbers]);
+  return (
+    <div>
+      <h3>This is App8</h3>
+      <p>
+        <input
+          type="number"
+          onChange={(e) => setNumber(e.target.value)}
+        ></input>
+        <button onClick={handleSubmit}>Add</button>
+      </p>
+      {numbers && numbers.map((value) => <div>{value}</div>)}
+      <hr></hr>
+      Total:{total}
+    </div>
+  );
 }
